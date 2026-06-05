798c9e6a2e31db8cae2dc068560f7b0c7cafc51c
kaidoracer-dev <killian.lb@protonmail.com>
2026-06-05 21:55:04 +0200

    fix

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,44 +11,35 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *tmp;
+    unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-{
+    if (*head == NULL)
+        return (-1);
+
     tmp = *head;
-    tmp->prev->next = tmp->next;
-    if (tmp->next != NULL)
-        tmp->next->prev = tmp->prev;
-    free(tmp);
-    *head = saved_head;
-}
-	return (1);
+    p = 0;
+    while (p < index && tmp != NULL)
+    {
+        tmp = tmp->next;
+        p++;
+    }
+    if (tmp == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = tmp->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(tmp);
+    }
+    else
+    {
+        tmp->prev->next = tmp->next;
+        if (tmp->next != NULL)
+            tmp->next->prev = tmp->prev;
+        free(tmp);
+    }
+    return (1);
 }
